05b345e70fb4b635ab296c84f2ea57098eab9a1d
Paul Adams <pauladams07@gmail.com>
2023-02-12 02:21:54 +0000

    added a code formatter. Hope I like it!

--- include/CardFinder.h
+++ include/CardFinder.h
@@ -1,43 +1,52 @@
 #ifndef CARD_FINDER_H
 #define CARD_FINDER_H
 
+#include "CardDatabase.h"
+#include "TableCard.h"
 #include <opencv2/core/core.hpp>
 #include <vector>
-#include "TableCard.h"
-#include "CardDatabase.h"
 
 class CardFinder
 {
-public:
-	CardFinder(CardDatabase * const cdb);
-	~CardFinder();
+  public:
+    CardFinder(CardDatabase *const cdb);
+    ~CardFinder();
 
-	std::vector<TableCard>* findAllCards(cv::Mat & scene);
-	void discernPartialCards();            /// TODO: could live in board manager class // needs to be called after frame colors are assigned.  more reason for new class
-	void reevaluateMemoryCards();
+    std::vector<TableCard> *findAllCards(cv::Mat &scene);
+    void discernPartialCards(); /// TODO: could live in board manager class // needs to be called after frame colors are
+                                /// assigned.  more reason for new class
+    void reevaluateMemoryCards();
 
-private:
-	CardDatabase* _cdb_ptr;
-	std::vector<TableCard> _foundCards;     /// TODO: could live in board manager class
-	std::vector<TableCard> _cardMemory; /// TODO: could live in board manager class
-	cv::Rect _biggestBox; // usually a player's hand
+  private:
+    CardDatabase *_cdb_ptr;
+    std::vector<TableCard> _foundCards; /// TODO: could live in board manager class
+    std::vector<TableCard> _cardMemory; /// TODO: could live in board manager class
+    cv::Rect _biggestBox;               // usually a player's hand
 
-	// constants
-	static const int MIN_AREA_ELIMINATION_THRESHOLD = 100;
+    // constants
+    static const int MIN_AREA_ELIMINATION_THRESHOLD = 100;
 
-	// functions for finding individual cards
-	cv::Mat findPlayField(const cv::Mat & scene) const;
-	void identifyCardsInRegion(const cv::Mat & ROI, const cv::Point ROIOffset, const cv::Rect & tableBB, std::vector<TableCard>& runningList) const;
-	TableCard extractCardImage(const cv::Mat & fromScene, const cv::RotatedRect boundingRect, const cv::Point worldPosition, const cv::Rect & tableBB, const bool isFullCard) const;
-	bool isContourConsumedByAnother(const std::vector<cv::Point> contour, const std::vector<cv::Point2f> consumedBy) const;
-	void outlineRotatedRectangle(cv::Mat & scene, const cv::RotatedRect RR, const cv::Scalar & color) const;
-	void blackoutRotatedRectangle(cv::Mat & scene, const cv::RotatedRect RR) const;
+    // functions for finding individual cards
+    cv::Mat findPlayField(const cv::Mat &scene) const;
+    void identifyCardsInRegion(const cv::Mat &ROI,
+                               const cv::Point ROIOffset,
+                               const cv::Rect &tableBB,
+                               std::vector<TableCard> &runningList) const;
+    TableCard extractCardImage(const cv::Mat &fromScene,
+                               const cv::RotatedRect boundingRect,
+                               const cv::Point worldPosition,
+                               const cv::Rect &tableBB,
+                               const bool isFullCard) const;
+    bool isContourConsumedByAnother(const std::vector<cv::Point> contour,
+                                    const std::vector<cv::Point2f> consumedBy) const;
+    void outlineRotatedRectangle(cv::Mat &scene, const cv::RotatedRect RR, const cv::Scalar &color) const;
+    void blackoutRotatedRectangle(cv::Mat &scene, const cv::RotatedRect RR) const;
 
-	// Trying things out part 1
-	void rememberNewCards();
-	void forgetOldCards();
-	bool recallCard(const TableCard& cardToRecall);
-	void evaluateCardsColors(std::vector<TableCard> & cards);
+    // Trying things out part 1
+    void rememberNewCards();
+    void forgetOldCards();
+    bool recallCard(const TableCard &cardToRecall);
+    void evaluateCardsColors(std::vector<TableCard> &cards);
 };
 
 #endif // CARD_FINDER_H
